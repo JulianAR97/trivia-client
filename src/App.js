1b6cfbe7bd2bf76876a9de7ae688f283845fe11d
Julian <julianrosenthal@yahoo.com>
2021-09-02 17:05:34 -0700

    style changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import Home from 'components/Home'
+
 const App = () => {
   return (
     <div className="App">
-      
+      <Home />      
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Container, Grid } from '@material-ui/core'
+import { Container, Grid, useMediaQuery, useTheme } from '@material-ui/core'
 import Game from 'components/Game/Game'
 import Stats from 'components/Stats/Stats'
 
 const Home = (props) => {
+  const theme = useTheme()
+  const isSmall = useMediaQuery(theme.breakpoints.down('sm'))
+  
   return (
     <Container>
-      <Grid container>
+      <Grid container spacing={isSmall ?  2 : 4}>
         <Stats />
         {/*Choose cat*/}
         <Game />
