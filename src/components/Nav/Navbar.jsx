38772f7a6c6e9023f23e2e4da27ee0a31496c3e7
Julian <julianrosenthal@yahoo.com>
2021-09-06 14:27:24 -0700

    updated navbar to login/signup

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { AppBar, IconButton, Toolbar, Typography, makeStyles } from '@material-ui/core'
+import { AppBar, Button, IconButton, Toolbar, Typography, makeStyles } from '@material-ui/core'
 import { Menu as MenuIcon} from '@material-ui/icons'
-
+import { useAuth } from 'contexts/AuthContext'
+import { useHistory } from 'react-router-dom'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -19,7 +20,17 @@ const useStyles = makeStyles((theme) => ({
 
 const Navbar = (props) => {
   const classes = useStyles()
-  
+  const { currentUser, logout } = useAuth()
+  const history = useHistory()
+
+  const handleAuthButton = async(e) => {
+    if (currentUser) {
+      await logout()
+      history.push('/')
+    } else {
+      history.push('/login')
+    }
+  }
 
   return (
     <AppBar position="static">
@@ -30,7 +41,11 @@ const Navbar = (props) => {
         <Typography variant="h6" className={classes.title}>
           News
         </Typography>
-        
+        <Button onClick={handleAuthButton}>
+          <Typography variant="subtitle2" className={classes.title}>
+            {currentUser ? 'logout' : 'login'}
+          </Typography>
+        </Button>
       </Toolbar>
     </AppBar>
   )
