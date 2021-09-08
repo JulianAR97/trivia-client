0b5f98fb442806e9ccc18f2d4f453b058a0be0d0
Julian <julianrosenthal@yahoo.com>
2021-09-08 14:46:57 -0700

    minor style edit

--- src/components/Game/Question.jsx
+++ src/components/Game/Question.jsx
@@ -5,6 +5,9 @@ const useStyles = makeStyles(theme => ({
   box: {
     textAlign: 'center',
     margin: '30px 0px'
+  },
+  typography: {
+    color: theme.palette.primary.light
   }
 }))
 
@@ -12,7 +15,7 @@ const Question = (props) => {
   const classes = useStyles()
   return (
     <Box className={classes.box}>
-      <Typography variant="h5">
+      <Typography variant="h5" color="primary" className={classes.typography}>
         {props.question}
       </Typography>
     </Box>
