40ef43a4f9629c9877ced10608ccc6bb3526a447
Julian <julianrosenthal@yahoo.com>
2021-09-08 14:21:47 -0700

    added media breakpoints

--- src/components/Game/Final.jsx
+++ src/components/Game/Final.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Grid, IconButton, Tooltip, Typography, makeStyles } from '@material-ui/core'
+import { Box, Grid, IconButton, Tooltip, Typography, makeStyles, useMediaQuery, useTheme } from '@material-ui/core'
 import { Replay } from '@material-ui/icons'
 import CircularProgressWithLabel from 'components/Misc/CircularProgressWithLabel'
 
@@ -24,11 +24,19 @@ const useStyles = makeStyles((theme) => ({
 
 const Final = (props) => {
   const classes = useStyles()
-  
-  const renderDifficulies = () => {
+  const theme = useTheme()
+  const isSmall = useMediaQuery(theme.breakpoints.down('xs'))
+
+  const renderDifficulties = () => {
     return Object.keys(props.difficultyCount).map((k, i) => (
       <Grid item className={classes.gridItem}>
-        <CircularProgressWithLabel key={i} value={`${props.difficultyCount[k].correct} / ${props.difficultyCount[k].seen}`} color="secondary" />
+        { /* value must be passed as number between 0 and 100, we pass 5 in if there are 0 correct answers so that progress indicator still shows */ } 
+        <CircularProgressWithLabel 
+          key={i}
+          value={Math.max(Math.round(props.difficultyCount[k].correct / props.difficultyCount[k].seen * 100), 5)}
+          textValue={`${props.difficultyCount[k].correct} / ${props.difficultyCount[k].seen}`} 
+          color="secondary" 
+        />
         <Typography className={classes.typography} variant="h6">
           {k}
         </Typography>
@@ -36,34 +44,45 @@ const Final = (props) => {
     ))
   }
   
+  const handleReplay = () => {
+    window.location.reload()
+  }
   
   return (
     <>
-      {/* Overall accuracy (percentage) */}
-      <Box className={classes.box}>
-        <Grid container className={classes.gridContainer}>
-          <Grid item>
-            <CircularProgressWithLabel 
-              value={`${(props.score / props.questionCount) * 100}%`} 
-              color="secondary" 
-            />
+      <Box>
+        <Grid container className={classes.gridContainer} spacing={10}>
+          <Grid item className={classes.gridItem}>
+            <Typography className={classes.typography} variant="h5">
+              Score
+            </Typography>
           </Grid>
         </Grid>
       </Box>
       
       {/* Accuracy per difficulty (fraction) */}
-      <Box className={classes.box}>
-        <Grid container className={classes.gridContainer} spacing={10}>
-          {renderDifficulies()}
+      <Box >
+        <Grid container className={classes.gridContainer} spacing={isSmall ? 3 : 10}>
+          {renderDifficulties()}
+          <Grid item className={classes.gridItem}>
+            <CircularProgressWithLabel 
+              value={Math.round((props.score / props.questionCount) * 100)}
+              textValue={`${(props.score / props.questionCount) * 100}%`} 
+              color="secondary" 
+            />
+            <Typography className={classes.typography} variant="h6">
+              accuracy
+            </Typography>
+          </Grid>
         </Grid>
       </Box>
 
       {/* Controls */}
-      <Box className={classes.box}>
+      <Box>
         <Grid container className={classes.gridContainer} spacing={10}>
-          <Grid item>
+          <Grid item className={classes.gridItem}>
             <Tooltip title="replay" arrow>
-              <IconButton>
+              <IconButton onClick={handleReplay}>
                 <Replay color="secondary" />
               </IconButton>
             </Tooltip>
