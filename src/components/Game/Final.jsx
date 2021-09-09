50ef1abb7cab6b7c43a00fa4ca5f7e35b8516e81
Julian <julianrosenthal@yahoo.com>
2021-09-09 14:31:30 -0700

    write to filestore functionality

--- src/components/Game/Final.jsx
+++ src/components/Game/Final.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box, Grid, IconButton, Tooltip, Typography, makeStyles, useMediaQuery, useTheme } from '@material-ui/core'
 import { Replay } from '@material-ui/icons'
 import CircularProgressWithLabel from 'components/Misc/CircularProgressWithLabel'
+import { database } from 'firebase.js'
+import { useAuth } from 'contexts/AuthContext'
 
 const useStyles = makeStyles((theme) => ({
   box: {
@@ -22,17 +24,76 @@ const useStyles = makeStyles((theme) => ({
 
 }))
 
+// get stats,
+// find or update stats
+
 const Final = (props) => {
   const classes = useStyles()
   const theme = useTheme()
   const isSmall = useMediaQuery(theme.breakpoints.down('xs'))
+  const { currentUser } = useAuth()
 
+  const createOrUpdateDoc = async () => {
+    const res = await database.stats.get({uid: currentUser.uid})
+    
+    if (res.docs[0]?.exists) {
+      // update doc
+      let docData = res.docs[0].data()
+
+      const result = await database.stats
+        .doc(res.docs[0].id)
+        .update({
+          totalQuestionCorrect: docData.totalQuestionCorrect + props.score,
+          totalQuestionCount: docData.totalQuestionCount + props.questionCount,
+          easyCorrect: docData.easyCorrect + props.difficultyCount['easy'].correct,
+          easyCount: docData.easyCount + props.difficultyCount['easy'].seen,
+          mediumCorrect: docData.mediumCorrect + props.difficultyCount['medium'].correct,
+          mediumCount: docData.mediumCount + props.difficultyCount['medium'].seen,
+          hardCorrect: docData.hardCorrect + props.difficultyCount['hard'].correct,
+          hardCount: docData.hardCount + props.difficultyCount['hard'].seen
+        })
+
+      return result
+    } else {
+      // create doc
+      console.log('here')
+      const result = await database.stats.add({
+        userId: currentUser.uid,
+        totalQuestionCorrect: props.score,
+        totalQuestionCount: props.questionCount,
+        easyCorrect: props.difficultyCount.easy.correct,
+        easyCount: props.difficultyCount.easy.seen,
+        mediumCorrect: props.difficultyCount['medium'].correct,
+        mediumCount: props.difficultyCount['medium'].seen,
+        hardCorrect: props.difficultyCount['hard'].correct,
+        hardCount: props.difficultyCount['hard'].seen
+      })
+      return result
+    }
+  }
+  console.log(props.difficultyCount)
+  
+  // OnMount, we want to update the stats
+  useEffect(() => {
+    // database.stats.add({
+    //   userId: currentUser.uid,
+    //   score: props.score,
+    //   questionCount: props.questionCount,
+    // })
+    
+    
+    createOrUpdateDoc()
+    
+   
+      
+    
+  })
+  
   const renderDifficulties = () => {
     return Object.keys(props.difficultyCount).map((k, i) => (
-      <Grid item className={classes.gridItem}>
+      <Grid item key={i} className={classes.gridItem}>
         { /* value must be passed as number between 0 and 100, we pass 5 in if there are 0 correct answers so that progress indicator still shows */ } 
         <CircularProgressWithLabel 
-          key={i}
           value={Math.max(Math.round(props.difficultyCount[k].correct / props.difficultyCount[k].seen * 100), 5)}
           textValue={`${props.difficultyCount[k].correct} / ${props.difficultyCount[k].seen}`} 
           color="secondary" 
@@ -47,6 +108,8 @@ const Final = (props) => {
   const handleReplay = () => {
     window.location.reload()
   }
+
+
   
   return (
     <>
