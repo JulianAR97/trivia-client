c405e16c6e87c2af6fe586b24d5ebbc7ca8c17ed
Julian <julianrosenthal@yahoo.com>
2021-09-09 17:00:26 -0700

    refactored

--- src/components/Stats/Accuracy.jsx
+++ src/components/Stats/Accuracy.jsx
@@ -43,8 +43,8 @@ const Accuracy = (props) => {
       <Grid item className={classes.gridItem}>
         { /* value must be passed as number between 0 and 100, we pass 5 in if there are 0 correct answers so that progress indicator still shows */ } 
         <CircularProgressWithLabel 
-          value={Math.max(Math.round(correct / count * 100), 5)}
-          textValue={`${correct} / ${count}`} 
+          value={Math.round(correct / count * 100)}
+          
           color="secondary" 
         />
         <Typography className={classes.typography} variant="h6">
@@ -63,17 +63,7 @@ const Accuracy = (props) => {
           {renderDifficulty(easyCorrect, easyCount, 'easy')}
           {renderDifficulty(mediumCorrect, mediumCount, 'medium')}
           {renderDifficulty(hardCorrect, hardCount, 'hard')}
-          
-          <Grid item className={classes.gridItem}>
-            <CircularProgressWithLabel 
-              value={Math.round((totalQuestionCorrect / totalQuestionCount) * 100)}
-              textValue={`${(totalQuestionCorrect / totalQuestionCount) * 100}%`} 
-              color="secondary" 
-            />
-            <Typography className={classes.typography} variant="h6">
-              accuracy
-            </Typography>
-          </Grid>
+          {renderDifficulty(totalQuestionCorrect, totalQuestionCount, 'accuracy')}
 
         </Grid>
       </Box>
