c9a0036e9ff1d0167f150abe18dda28e1e81ed6f
Julian <julianrosenthal@yahoo.com>
2021-09-14 14:51:18 -0700

    added setProfile function

--- src/actions/Profile.js
+++ src/actions/Profile.js
@@ -5,8 +5,20 @@ const getProfile = async (currentUser) => {
   return profile
 }
 
+const setProfile = async (currentUser) => {
+  let profile = await database.profile.where('userId', '==', currentUser.uid).get()
+  // if user doesn't have a profile doc, add one
+  if (!profile.docs[0]) {
+    database.profile.add({
+      userId: currentUser.uid,
+      avatar: ''
+    })
+  }
+}
+
 export {
   getProfile,
+  setProfile
 }
 
 
