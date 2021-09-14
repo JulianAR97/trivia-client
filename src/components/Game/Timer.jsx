39fe67442085a6f956d5639f123d46340d5c4c25
Julian <julianrosenthal@yahoo.com>
2021-09-14 10:31:43 -0700

    added timer style

--- src/components/Game/Timer.jsx
+++ src/components/Game/Timer.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
+import { useTheme } from '@material-ui/core' 
 import CircularProgressWithLabel from 'components/Misc/CircularProgressWithLabel'
 import { useTimer } from 'react-timer-hook';
 
 const Timer = ({ expiryTimestamp }) => {
+  const theme = useTheme()
+  
   const {
     seconds,
     restart
@@ -11,10 +14,15 @@ const Timer = ({ expiryTimestamp }) => {
   
   useEffect(() => {
     restart(expiryTimestamp)
-  })
+  }, [expiryTimestamp])
 
+  console.log(theme.palette.secondary.main)
   return (
-    <CircularProgressWithLabel value={seconds * 3.34} label={seconds} />
+    <CircularProgressWithLabel 
+      color={ seconds > 10 ? 'primary' : 'secondary' }
+      value={seconds * 3.34} 
+      label={seconds} 
+    />
     
   )
 }
