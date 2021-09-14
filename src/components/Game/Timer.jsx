9d5dcbc3b7c0144b9fc1cd2a8c2928aefd6e4f7b
Julian <julianrosenthal@yahoo.com>
2021-09-14 10:15:12 -0700

    added timer component

--- src/components/Game/Timer.jsx
+++ src/components/Game/Timer.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import CircularProgressWithLabel from 'components/Misc/CircularProgressWithLabel'
+import { useTimer } from 'react-timer-hook';
 
-const Timer = (props) => {
+const Timer = ({ expiryTimestamp }) => {
+  const {
+    seconds,
+    restart
+  } = useTimer({expiryTimestamp})
+  
+  
+  useEffect(() => {
+    restart(expiryTimestamp)
+  })
+
   return (
-    <>
-    </>
+    <CircularProgressWithLabel value={seconds * 3.34} label={seconds} />
+    
   )
 }
+
 
 export default Timer
