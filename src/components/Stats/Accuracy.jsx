7d0bc6dc221303ac06799c3b104922e0f378d63b
Julian <julianrosenthal@yahoo.com>
2021-09-14 10:31:57 -0700

    fixed label for circular progress

--- src/components/Stats/Accuracy.jsx
+++ src/components/Stats/Accuracy.jsx
@@ -44,7 +44,7 @@ const Accuracy = (props) => {
         { /* value must be passed as number between 0 and 100, we pass 5 in if there are 0 correct answers so that progress indicator still shows */ } 
         <CircularProgressWithLabel 
           value={Math.round(correct / count * 100)}
-          
+          label={`${Math.round(correct / count * 100)}%`}
           color="secondary" 
         />
         <Typography className={classes.typography} variant="h6">
