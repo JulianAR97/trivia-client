8d5707c857998185283259d496e5c2a05aa1508f
Julian <julianrosenthal@yahoo.com>
2021-09-14 18:10:59 -0700

    added spacing

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box, Card, Container, Grid, Typography, makeStyles, useMediaQuery, useTheme } from '@material-ui/core'
 import ProfileNav from 'components/Profile/ProfileNav.jsx'
 
+
 const useStyles = makeStyles((theme) => ({
   grid: {
     textAlign: 'center'
@@ -30,20 +31,21 @@ const useStyles = makeStyles((theme) => ({
   card: {
     marginTop: '20px',
     textAlign: 'center',
-  }
+  },
 }))
 
 const Profile = (props) => {
   const classes = useStyles()
   const theme = useTheme()
   const isSmall = useMediaQuery(theme.breakpoints.down('sm'))
-
+  
 
   const calcCardStyle = () => {
     return {
       padding: isSmall ? '30px 10px' : '30px'
     }
   }
+  
 
   return (
     
@@ -51,11 +53,12 @@ const Profile = (props) => {
       <Card
         className={classes.card}
         style={calcCardStyle()}
+        raised
       >
         <Grid item xs={12} className={classes.grid}>
           <Box>
             <Grid container className={classes.gridContainer} spacing={10}>
-              <Grid item clasName={classes.gridItem}>
+              <Grid item className={classes.gridItem}>
                 <Typography className={classes.typography} variant="h5">
                   Profile
                 </Typography>
@@ -64,7 +67,9 @@ const Profile = (props) => {
           </Box>
         
         </Grid>
+        
         <ProfileNav />
+        
       </Card>
     </Container>
 
